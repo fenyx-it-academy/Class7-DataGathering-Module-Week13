82c865015de71ce54ba7961e581d4433a0e83a74
Semih <46994029+sigma21@users.noreply.github.com>
2022-12-17 20:19:23 +0100

    project scaffold

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Class7-DataGathering-Module-Week13
+
+ISS Location Reminder App
+
+The goal of this assignment is to create an app which checks the location of the ISS and notifies the user if it is visible in user's area.
+
+Check the `main.py` file and add the necessary codes in lines mentioned.
+
+Good luck! 🙂
---
+++ main.py
@@ -0,0 +1,112 @@
+import os
+from datetime import datetime
+import smtplib
+import time
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# TODO! Go to https://www.latlong.net/convert-address-to-lat-long.html and type in your address to get your location
+# Store the latitude and longitude values in the variables below
+MY_LAT = "?"
+MY_LONG = "?"
+
+
+
+def is_iss_overhead():
+    """
+        Sends a GET request to ISS API to locate its lat and long.
+    Returns:
+        bool: Returns True if user's position is within +/-5 degrees of ISS's location, otherwise returns False.
+    """
+    
+    # TODO! Make an API call (a GET request) to "http://api.open-notify.org/iss-now.json"
+    # <your code here>
+    
+    # TODO! Check for any errors by using the raise_for_status method
+    # <your code here>
+
+    # TODO! Store the JSON representation of the response object in a variable
+    # <your code here>
+
+    # TODO! Parse the response object and store latitude and longitude information in variables below
+    iss_latitude = "<your code here>"
+    iss_longitude = "<your code here>"
+
+    #Return True if user's position is within +5 or -5 degrees of the ISS position.
+    if (MY_LAT-5 <= float(iss_latitude) <= MY_LAT+5) and (MY_LONG-5 <= float(iss_longitude) <= MY_LONG+5):
+        return True
+    
+    return False
+
+
+def is_night_time():
+    """
+        Sends a GET request to get the sunrise and sunset time of a location.
+    Returns:
+        bool: True if it is currently night time, False if day time.
+    """
+
+    # TODO! Check out the API documentation at https://sunrise-sunset.org/api
+    # Populate the parameters object below by adding the required parameters
+    # IMPORTANT! Make sure to keep the "formatted" parameter as 0 to get the time value in ISO format. 
+    parameters = {
+        "?" : "?",
+        "?": "?",
+        "formatted": 0,
+    }
+
+    # TODO! Make an API call (a GET request) to "https://api.sunrise-sunset.org/json" along with the parameters object above.
+    # Check out documentation of requests library to learn how to add parameters as a separate object in a GET request.
+    # Hint: The secret info is somewhere in this page 🧐 -->  https://requests.readthedocs.io/en/latest/user/quickstart/
+    # <your code here>
+
+    
+
+    # TODO! Check for any errors by using the raise_for_status method
+    # <your code here>
+
+    # TODO! Store the JSON representation of the response object in a variable
+    # <your code here>
+
+    # TODO! Parse the response object and store sunrise and sunset information in variables below
+    sunrise = "<your code here>"
+    sunset = "<your code here>"
+
+    # Get the current hour
+    time_now = datetime.now().hour
+
+    # Return True if it is night time
+    if time_now >= int(sunset.split("T")[1].split(":")[0]) or time_now <= int(sunrise.split("T")[1].split(":")[0]):
+        return True
+    
+    return False
+
+
+# Main app logic:
+# If the ISS is close to your current position and it is currently night time, notify the user
+while True:
+    if is_iss_overhead() and is_night_time():
+        print("Look Up👆\n\nThe ISS is above you in the sky.")
+    else:    
+        print("Unfortunately the ISS is not visible at this time, checking again in 60 seconds...")
+    time.sleep(60)
+
+
+
+# Alternative method with SMTP library to send emails
+# If you want to use this method and send emails, add your email and password into the .env file attached to the project
+
+# while True:
+#     if is_iss_overhead() and is_night_time():
+#         connection = smtplib.SMTP("__YOUR_SMTP_ADDRESS_HERE___") # look for the correct smtp address for your email service (gmail, outlook etc.)
+#         connection.starttls()
+#         connection.login(os.getenv("MY_EMAIL"), os.getenv("MY_PASSWORD"))
+#         connection.sendmail(
+#             from_addr=os.getenv("MY_EMAIL"),
+#             to_addrs=os.getenv("MY_EMAIL"),
+#             msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
+#         )
+#     time.sleep(60)
