de2f3391e7b1f6919abb727c510086f24ea9de86
SefaSAHAN <sefasahan35@gmail.com>
2022-12-20 19:36:38 +0100

    homework done

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ load_dotenv()
 
 # TODO! Go to https://www.latlong.net/convert-address-to-lat-long.html and type in your address to get your location
 # Store the latitude and longitude values in the variables below
-MY_LAT = "?"
-MY_LONG = "?"
+MY_LAT = -20
+MY_LONG = 110
 
 
 
@@ -32,8 +32,19 @@ def is_iss_overhead():
     # <your code here>
 
     # TODO! Parse the response object and store latitude and longitude information in variables below
-    iss_latitude = "<your code here>"
-    iss_longitude = "<your code here>"
+    
+    try:
+       
+            response = requests.get("http://api.open-notify.org/iss-now.json")
+            response.raise_for_status()
+            data = response.json()
+            print(data)
+           
+
+    except requests.exceptions.TooManyRedirects as error:
+        print(error)
+    iss_latitude = data["iss_position"]["latitude"]
+    iss_longitude = data["iss_position"]["longitude"]
 
     #Return True if user's position is within +5 or -5 degrees of the ISS position.
     if (MY_LAT-5 <= float(iss_latitude) <= MY_LAT+5) and (MY_LONG-5 <= float(iss_longitude) <= MY_LONG+5):
@@ -57,7 +68,16 @@ def is_night_time():
         "?": "?",
         "formatted": 0,
     }
+    try:
+       
+            response = requests.get(f"https://api.sunrise-sunset.org/json?lat={MY_LAT}&lng={MY_LONG}&formatted=0")
+            response.raise_for_status()
+            data = response.json()
+            print(data)
+           
 
+    except requests.exceptions.TooManyRedirects as error:
+        print(error)
     # TODO! Make an API call (a GET request) to "https://api.sunrise-sunset.org/json" along with the parameters object above.
     # Check out documentation of requests library to learn how to add parameters as a separate object in a GET request.
     # Hint: The secret info is somewhere in this page 🧐 -->  https://requests.readthedocs.io/en/latest/user/quickstart/
@@ -72,8 +92,8 @@ def is_night_time():
     # <your code here>
 
     # TODO! Parse the response object and store sunrise and sunset information in variables below
-    sunrise = "<your code here>"
-    sunset = "<your code here>"
+    sunrise = data["results"]["sunrise"]
+    sunset = data["results"]["sunset"]
 
     # Get the current hour
     time_now = datetime.now().hour
@@ -93,6 +113,7 @@ while True:
     else:    
         print("Unfortunately the ISS is not visible at this time, checking again in 60 seconds...")
     time.sleep(60)
+
 
 
 
