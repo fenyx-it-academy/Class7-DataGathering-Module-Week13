e02a4e78f70c734a2264113a8e7b8ce1745492fe
Juan Obregon <jdor1262@gmail.com>
2022-12-23 01:05:07 +0100

    HW-w13

--- main.py
+++ main.py
@@ -10,9 +10,8 @@ load_dotenv()
 
 # TODO! Go to https://www.latlong.net/convert-address-to-lat-long.html and type in your address to get your location
 # Store the latitude and longitude values in the variables below
-MY_LAT = "?"
-MY_LONG = "?"
-
+MY_LAT = float(51.089770)
+MY_LONG = float(5.911940)
 
 
 def is_iss_overhead():
@@ -27,64 +26,68 @@ def is_iss_overhead():
     
     # TODO! Check for any errors by using the raise_for_status method
     # <your code here>
-
     # TODO! Store the JSON representation of the response object in a variable
     # <your code here>
 
     # TODO! Parse the response object and store latitude and longitude information in variables below
-    iss_latitude = "<your code here>"
-    iss_longitude = "<your code here>"
+
+    try:
+        response = requests.get("http://api.open-notify.org/iss-now.json")
+        response.raise_for_status()
+        # Code here will only run if the request is successful
+    except requests.exceptions.TooManyRedirects as error:
+        print(error)
+
+    iss_latitude = response.json()['iss_position']['longitude']   
+    iss_longitude =response.json()['iss_position']['latitude'] 
 
     #Return True if user's position is within +5 or -5 degrees of the ISS position.
     if (MY_LAT-5 <= float(iss_latitude) <= MY_LAT+5) and (MY_LONG-5 <= float(iss_longitude) <= MY_LONG+5):
         return True
     
     return False
-
-
 def is_night_time():
     """
         Sends a GET request to get the sunrise and sunset time of a location.
     Returns:
         bool: True if it is currently night time, False if day time.
     """
-
     # TODO! Check out the API documentation at https://sunrise-sunset.org/api
     # Populate the parameters object below by adding the required parameters
     # IMPORTANT! Make sure to keep the "formatted" parameter as 0 to get the time value in ISO format. 
     parameters = {
         "?" : "?",
         "?": "?",
+        "lat" : MY_LAT ,
+        "lng":MY_LONG ,
         "formatted": 0,
     }
+
+
 
     # TODO! Make an API call (a GET request) to "https://api.sunrise-sunset.org/json" along with the parameters object above.
     # Check out documentation of requests library to learn how to add parameters as a separate object in a GET request.
     # Hint: The secret info is somewhere in this page 🧐 -->  https://requests.readthedocs.io/en/latest/user/quickstart/
     # <your code here>
 
-    
+    try:
+            response = requests.get(f"https://api.sunrise-sunset.org/json",params=parameters)
+            response.raise_for_status()
+    except requests.exceptions.TooManyRedirects as error:
+            print(error)
 
-    # TODO! Check for any errors by using the raise_for_status method
-    # <your code here>
 
-    # TODO! Store the JSON representation of the response object in a variable
-    # <your code here>
+    sunrise =response.json()['results']['sunrise']
+    sunset = response.json()['results']['sunset']
 
-    # TODO! Parse the response object and store sunrise and sunset information in variables below
-    sunrise = "<your code here>"
-    sunset = "<your code here>"
 
     # Get the current hour
     time_now = datetime.now().hour
-
     # Return True if it is night time
     if time_now >= int(sunset.split("T")[1].split(":")[0]) or time_now <= int(sunrise.split("T")[1].split(":")[0]):
         return True
     
     return False
-
-
 # Main app logic:
 # If the ISS is close to your current position and it is currently night time, notify the user
 while True:
@@ -93,16 +96,12 @@ while True:
     else:    
         print("Unfortunately the ISS is not visible at this time, checking again in 60 seconds...")
     time.sleep(60)
-
-
-
 # Alternative method with SMTP library to send emails
 # If you want to use this method and send emails follow the steps below:
 # create a .env file in your project directory
 # add your email and password into the .env file by copying the below 2 lines and changing them with real values
 # MY_EMAIL = "___YOUR_EMAIL_HERE____"
 # MY_PASSWORD = "___YOUR_PASSWORD_HERE___"
-
 # while True:
 #     if is_iss_overhead() and is_night_time():
 #         connection = smtplib.SMTP("__YOUR_SMTP_ADDRESS_HERE___") # look for the correct smtp address for your email service (gmail, outlook etc.)
