ef467df1e5b3b04e13ad35091a989b8cf91d0d56
HaftomZ <122086208+HaftomZ@users.noreply.github.com>
2023-04-11 18:23:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ load_dotenv()
 
 # TODO! Go to https://www.latlong.net/convert-address-to-lat-long.html and type in your address to get your location
 # Store the latitude and longitude values in the variables below
-MY_LAT = "?"
-MY_LONG = "?"
+MY_LAT = 52.410049
+MY_LONG = 4.862280
 
 
 
@@ -24,23 +24,24 @@ def is_iss_overhead():
     
     # TODO! Make an API call (a GET request) to "http://api.open-notify.org/iss-now.json"
     # <your code here>
-    
+    result = requests.get('http://api.open-notify.org/iss-now.json')
     # TODO! Check for any errors by using the raise_for_status method
     # <your code here>
+    result.raise_for_status()
+    response = result.json()
+        
+     # TODO! Store the JSON representation of the response object in a variable
+     # <your code here>
 
-    # TODO! Store the JSON representation of the response object in a variable
-    # <your code here>
+     # TODO! Parse the response object and store latitude and longitude information in variables below
+    iss_latitude = response['iss_position']['latitude']
+    iss_longitude = response['iss_position']['longitude']
 
-    # TODO! Parse the response object and store latitude and longitude information in variables below
-    iss_latitude = "<your code here>"
-    iss_longitude = "<your code here>"
-
-    #Return True if user's position is within +5 or -5 degrees of the ISS position.
+     #Return True if user's position is within +5 or -5 degrees of the ISS position.
     if (MY_LAT-5 <= float(iss_latitude) <= MY_LAT+5) and (MY_LONG-5 <= float(iss_longitude) <= MY_LONG+5):
         return True
     
     return False
-
 
 def is_night_time():
     """
@@ -53,8 +54,8 @@ def is_night_time():
     # Populate the parameters object below by adding the required parameters
     # IMPORTANT! Make sure to keep the "formatted" parameter as 0 to get the time value in ISO format. 
     parameters = {
-        "?" : "?",
-        "?": "?",
+        "lat" : MY_LAT,
+        "lng": MY_LONG,
         "formatted": 0,
     }
 
@@ -62,23 +63,25 @@ def is_night_time():
     # Check out documentation of requests library to learn how to add parameters as a separate object in a GET request.
     # Hint: The secret info is somewhere in this page 🧐 -->  https://requests.readthedocs.io/en/latest/user/quickstart/
     # <your code here>
+    result = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
 
     
 
     # TODO! Check for any errors by using the raise_for_status method
     # <your code here>
-
+    result.raise_for_status()
     # TODO! Store the JSON representation of the response object in a variable
     # <your code here>
-
+    response = result.json()
+    print(response)
     # TODO! Parse the response object and store sunrise and sunset information in variables below
-    sunrise = "<your code here>"
-    sunset = "<your code here>"
+    sunrise = response['results']['sunrise']
+    sunset = response['results']['sunset']
 
     # Get the current hour
     time_now = datetime.now().hour
 
-    # Return True if it is night time
+    #Return True if it is night time
     if time_now >= int(sunset.split("T")[1].split(":")[0]) or time_now <= int(sunrise.split("T")[1].split(":")[0]):
         return True
     
@@ -88,7 +91,7 @@ def is_night_time():
 # Main app logic:
 # If the ISS is close to your current position and it is currently night time, notify the user
 while True:
-    if is_iss_overhead() and is_night_time():
+    if is_iss_overhead()and is_night_time():
         print("Look Up👆\n\nThe ISS is above you in the sky.")
     else:    
         print("Unfortunately the ISS is not visible at this time, checking again in 60 seconds...")
